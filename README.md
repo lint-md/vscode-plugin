dd38abf596a6754626cc4853d037e1ba591d7f47
hjio <zhixiang@live.cn>
2018-12-24 12:08:05 +0800

    build 0001

--- README.md
+++ README.md
@@ -1,38 +1,24 @@
-# LSP Example
+# vscode-mdlint-plugin
+**2018/12/24, build 0001 (preview edition)**
 
-Heavily documented sample code for https://code.visualstudio.com/api/language-extensions/language-server-extension-guide
+vscode-mdlint-plugin is an awesome vscode markdown plugin, powerred by [hustcc/lint-md](https://github.com/hustcc/lint-md)
 
-## Functionality
+## How to use
+- search `'mdlint'` and install it.
+- create the configuration file and name it `'mdlint.json'`.
+- or you have no specical preference? Just keep no config file, `mdlint` will work still fine in `recommanded mode`.
 
-This Language Server works for plain text file. It has the following language features:
-- Completions
-- Diagnostics regenerated on each file change or configuration change
-
-It also includes an End-to-End test.
-
-## Structure
-
+##  `mdlint.json`
+### format
+```json
+{
+  "rules": {
+    "no-empty-code": 1
+    // RULE: LEVEL
+  }
+}
 ```
-.
-├── client // Language Client
-│   ├── src
-│   │   ├── test // End to End tests for Language Client / Server
-│   │   └── extension.ts // Language Client entry point
-├── package.json // The extension manifest.
-└── server // Language Server
-    └── src
-        └── server.ts // Language Server entry point
-```
+### API
+see [hustcc/lint-md](https://github.com/hustcc/lint-md) for more.
 
-## Running the Sample
 
-- Run `npm install` in this folder. This installs all necessary npm modules in both the client and server folder
-- Open VS Code on this folder.
-- Press Ctrl+Shift+B to compile the client and server.
-- Switch to the Debug viewlet.
-- Select `Launch Client` from the drop down.
-- Run the lauch config.
-- If you want to debug the server as well use the launch configuration `Attach to Server`
-- In the [Extension Development Host] instance of VSCode, open a document in 'plain text' language mode.
-  - Type `j` or `t` to see `Javascript` and `TypeScript` completion.
-  - Enter text content such as `AAA aaa BBB`. The extension will emit diagnostics for all words in all-uppercase.
--- server/src/server.ts
+++ server/src/server.ts
@@ -4,214 +4,237 @@
  * ------------------------------------------------------------------------------------------ */
 
 import {
-	createConnection,
-	TextDocuments,
-	TextDocument,
-	Diagnostic,
-	DiagnosticSeverity,
-	ProposedFeatures,
-	InitializeParams,
-	DidChangeConfigurationNotification,
-	CompletionItem,
-	CompletionItemKind,
-	TextDocumentPositionParams,
-	Position
-} from 'vscode-languageserver';
-import { lint, version } from 'lint-md';
-
-
+  createConnection,
+  TextDocuments,
+  TextDocument,
+  Diagnostic,
+  DiagnosticSeverity,
+  ProposedFeatures,
+  InitializeParams,
+  DidChangeConfigurationNotification,
+  CompletionItem,
+  CompletionItemKind,
+  TextDocumentPositionParams,
+  Position
+} from 'vscode-languageserver'
+import * as path from 'path'
+import * as fs from 'fs'
+import { lint, version } from 'lint-md'
+let rulesConfig = {}
+if (fs.existsSync(path.resolve('./mdlint.json'))) {
+  rulesConfig = require(path.resolve('./mdlint.json')).rules
+}
+const getLevelType = (level: string) => {
+  switch (level) {
+    case 'error':
+      return 'Error'
+    case 'warning':
+      return 'Warning'
+    case 'ignore':
+      return false
+  }
+}
 // Create a connection for the server. The connection uses Node's IPC as a transport.
 // Also include all preview / proposed LSP features.
-let connection = createConnection(ProposedFeatures.all);
+let connection = createConnection(ProposedFeatures.all)
 
 // Create a simple text document manager. The text document manager
 // supports full document sync only
-let documents: TextDocuments = new TextDocuments();
+let documents: TextDocuments = new TextDocuments()
 
-let hasConfigurationCapability: boolean = false;
-let hasWorkspaceFolderCapability: boolean = false;
-let hasDiagnosticRelatedInformationCapability: boolean = false;
+let hasConfigurationCapability: boolean = false
+let hasWorkspaceFolderCapability: boolean = false
+let hasDiagnosticRelatedInformationCapability: boolean = false
 
 connection.onInitialize((params: InitializeParams) => {
-	let capabilities = params.capabilities;
+  let capabilities = params.capabilities
 
-	// Does the client support the `workspace/configuration` request?
-	// If not, we will fall back using global settings
-	hasConfigurationCapability = !!(capabilities.workspace && !!capabilities.workspace.configuration);
-	hasWorkspaceFolderCapability = !!(capabilities.workspace && !!capabilities.workspace.workspaceFolders);
-	hasDiagnosticRelatedInformationCapability =
-		!!(capabilities.textDocument &&
-		capabilities.textDocument.publishDiagnostics &&
-		capabilities.textDocument.publishDiagnostics.relatedInformation);
+  // Does the client support the `workspace/configuration` request?
+  // If not, we will fall back using global settings
+  hasConfigurationCapability = !!(
+    capabilities.workspace && !!capabilities.workspace.configuration
+  )
+  hasWorkspaceFolderCapability = !!(
+    capabilities.workspace && !!capabilities.workspace.workspaceFolders
+  )
+  hasDiagnosticRelatedInformationCapability = !!(
+    capabilities.textDocument &&
+    capabilities.textDocument.publishDiagnostics &&
+    capabilities.textDocument.publishDiagnostics.relatedInformation
+  )
 
-	return {
-		capabilities: {
-			textDocumentSync: documents.syncKind,
-			// Tell the client that the server supports code completion
-			completionProvider: {
-				resolveProvider: true
-			}
-		}
-	};
-});
+  return {
+    capabilities: {
+      textDocumentSync: documents.syncKind,
+      // Tell the client that the server supports code completion
+      completionProvider: {
+        resolveProvider: true
+      }
+    }
+  }
+})
 
 connection.onInitialized(() => {
-	if (hasConfigurationCapability) {
-		// Register for all configuration changes.
-		connection.client.register(
-			DidChangeConfigurationNotification.type,
-			undefined
-		);
-	}
-	if (hasWorkspaceFolderCapability) {
-		connection.workspace.onDidChangeWorkspaceFolders(_event => {
-			connection.console.log('Workspace folder change event received.');
-		});
-	}
-});
+  if (hasConfigurationCapability) {
+    // Register for all configuration changes.
+    connection.client.register(
+      DidChangeConfigurationNotification.type,
+      undefined
+    )
+  }
+  if (hasWorkspaceFolderCapability) {
+    connection.workspace.onDidChangeWorkspaceFolders(_event => {
+      connection.console.log('Workspace folder change event received.')
+    })
+  }
+})
 
 // The example settings
 interface ExampleSettings {
-	maxNumberOfProblems: number;
+  maxNumberOfProblems: number
 }
 
 // The global settings, used when the `workspace/configuration` request is not supported by the client.
 // Please note that this is not the case when using this server with the client provided in this example
 // but could happen with other clients.
-const defaultSettings: ExampleSettings = { maxNumberOfProblems: 1000 };
-let globalSettings: ExampleSettings = defaultSettings;
+const defaultSettings: ExampleSettings = { maxNumberOfProblems: 1000 }
+let globalSettings: ExampleSettings = defaultSettings
 
 // Cache the settings of all open documents
-let documentSettings: Map<string, Thenable<ExampleSettings>> = new Map();
+let documentSettings: Map<string, Thenable<ExampleSettings>> = new Map()
 
 connection.onDidChangeConfiguration(change => {
-	if (hasConfigurationCapability) {
-		// Reset all cached document settings
-		documentSettings.clear();
-	} else {
-		globalSettings = <ExampleSettings>(
-			(change.settings.languageServerExample || defaultSettings)
-		);
-	}
+  if (hasConfigurationCapability) {
+    // Reset all cached document settings
+    documentSettings.clear()
+  } else {
+    globalSettings = <ExampleSettings>(
+      (change.settings.languageServerExample || defaultSettings)
+    )
+  }
 
-	// Revalidate all open text documents
-	documents.all().forEach(validateTextDocument);
-});
-
+  // Revalidate all open text documents
+  documents.all().forEach(validateTextDocument)
+})
 function getDocumentSettings(resource: string): Thenable<ExampleSettings> {
-	if (!hasConfigurationCapability) {
-		return Promise.resolve(globalSettings);
-	}
-	let result = documentSettings.get(resource);
-	if (!result) {
-		result = connection.workspace.getConfiguration({
-			scopeUri: resource,
-			section: 'languageServerExample'
-		});
-		documentSettings.set(resource, result);
-	}
-	return result;
+  if (!hasConfigurationCapability) {
+    return Promise.resolve(globalSettings)
+  }
+  let result = documentSettings.get(resource)
+  if (!result) {
+    result = connection.workspace.getConfiguration({
+      scopeUri: resource,
+      section: 'mdlint'
+    })
+    documentSettings.set(resource, result)
+  }
+  return result
 }
 
 // Only keep settings for open documents
 documents.onDidClose(e => {
-	documentSettings.delete(e.document.uri);
-});
+  documentSettings.delete(e.document.uri)
+})
 
 // The content of a text document has changed. This event is emitted
 // when the text document first opened or when its content has changed.
 documents.onDidChangeContent(change => {
-	validateTextDocument(change.document);
-});
+  validateTextDocument(change.document)
+  // fs.readFile(path.resolve('./.mdlintrc'), (data: any)=>{
+  // 	// let rulesConfig = JSON.parse(data.toString()).rules
+  // 	let rulesConfig = require(data.toString()).rules
+  // 	validateTextDocument(change.document, rulesConfig)
+  // })
+})
 
 async function validateTextDocument(textDocument: TextDocument): Promise<void> {
-	// In this simple example we get the settings for every validate run.
-	let settings = await getDocumentSettings(textDocument.uri);
+  // In this simple example we get the settings for every validate run.
+  let settings = await getDocumentSettings(textDocument.uri)
 
-	// The validator creates diagnostics for all uppercase words length 2 and more
-	let text = textDocument.getText();
-	const errors = lint(text, {});
-	let problems = 0;
-	let diagnostics: Diagnostic[] = [];
-	errors.forEach((error: any) => {
-		const {start, end, text} = error
-		problems++;
-		let diagnosic: Diagnostic = {
-			severity: DiagnosticSeverity.Error,
-			range: {
-				start: Position.create(start.line - 1,start.column -1 ),
-				end: Position.create(end.line - 1,end.column -1 )
-				// start: textDocument.positionAt(m.index),
-				// end: textDocument.positionAt(m.index + m[0].length)
-			},
-			message: text + '...',
-			source: 'mdlint'
-		};
-		// if (hasDiagnosticRelatedInformationCapability) {
-		// 	diagnosic.relatedInformation = [
-		// 		{
-		// 			location: {
-		// 				uri: textDocument.uri,
-		// 				range: Object.assign({}, diagnosic.range)
-		// 			},
-		// 			message: 'Spelling matters'
-		// 		},
-		// 		{
-		// 			location: {
-		// 				uri: textDocument.uri,
-		// 				range: Object.assign({}, diagnosic.range)
-		// 			},
-		// 			message: 'Particularly for names'
-		// 		}
-		// 	];
-		// }
-		diagnostics.push(diagnosic);
-	})
+  // The validator creates diagnostics for all uppercase words length 2 and more
+  let text = textDocument.getText()
+  const errors = lint(text, rulesConfig)
+  let diagnostics: Diagnostic[] = []
+  errors.forEach((error: any) => {
+    const { start, end, text, type, level } = error
+    const levelType = getLevelType(level)
+    if (levelType) {
+      let diagnosic: Diagnostic = {
+        severity: DiagnosticSeverity[levelType],
+        range: {
+          start: Position.create(start.line - 1, start.column - 1),
+          end: Position.create(end.line - 1, end.column - 1)
+        },
+        message: `${text}\n[${type}]`,
+        source: 'mdlint'
+      }
+      diagnostics.push(diagnosic)
+    }
 
-	// Send the computed diagnostics to VSCode.
-	connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
+    // if (hasDiagnosticRelatedInformationCapability) {
+    // 	diagnosic.relatedInformation = [
+    // 		{
+    // 			location: {
+    // 				uri: textDocument.uri,
+    // 				range: Object.assign({}, diagnosic.range)
+    // 			},
+    // 			message: 'Spelling matters'
+    // 		},
+    // 		{
+    // 			location: {
+    // 				uri: textDocument.uri,
+    // 				range: Object.assign({}, diagnosic.range)
+    // 			},
+    // 			message: 'Particularly for names'
+    // 		}
+    // 	];
+    // }
+  })
+
+  // Send the computed diagnostics to VSCode.
+  connection.sendDiagnostics({ uri: textDocument.uri, diagnostics })
 }
 
-connection.onDidChangeWatchedFiles(_change => {
-	// Monitored files have change in VSCode
-	connection.console.log('We received an file change event');
-});
+// connection.onDidChangeWatchedFiles(_change => {
+// 	// Monitored files have change in VSCode
+// 	connection.console.log('We received an file change event');
+// });
 
-// This handler provides the initial list of the completion items.
-connection.onCompletion(
-	(_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
-		// The pass parameter contains the position of the text document in
-		// which code complete got requested. For the example we ignore this
-		// info and always provide the same completion items.
-		return [
-			{
-				label: 'TypeScript',
-				kind: CompletionItemKind.Text,
-				data: 1
-			},
-			{
-				label: 'JavaScript',
-				kind: CompletionItemKind.Text,
-				data: 2
-			}
-		];
-	}
-);
+// // This handler provides the initial list of the completion items.
+// connection.onCompletion(
+// 	(_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+// 		// The pass parameter contains the position of the text document in
+// 		// which code complete got requested. For the example we ignore this
+// 		// info and always provide the same completion items.
+// 		return [
+// 			{
+// 				label: 'TypeScript',
+// 				kind: CompletionItemKind.Text,
+// 				data: 1
+// 			},
+// 			{
+// 				label: 'JavaScript',
+// 				kind: CompletionItemKind.Text,
+// 				data: 2
+// 			}
+// 		];
+// 	}
+// );
 
-// This handler resolves additional information for the item selected in
-// the completion list.
-connection.onCompletionResolve(
-	(item: CompletionItem): CompletionItem => {
-		if (item.data === 1) {
-			(item.detail = 'TypeScript details'),
-				(item.documentation = 'TypeScript documentation');
-		} else if (item.data === 2) {
-			(item.detail = 'JavaScript details'),
-				(item.documentation = 'JavaScript documentation');
-		}
-		return item;
-	}
-);
+// // This handler resolves additional information for the item selected in
+// // the completion list.
+// connection.onCompletionResolve(
+// 	(item: CompletionItem): CompletionItem => {
+// 		if (item.data === 1) {
+// 			(item.detail = 'TypeScript details'),
+// 				(item.documentation = 'TypeScript documentation');
+// 		} else if (item.data === 2) {
+// 			(item.detail = 'JavaScript details'),
+// 				(item.documentation = 'JavaScript documentation');
+// 		}
+// 		return item;
+// 	}
+// );
 
 /*
 connection.onDidOpenTextDocument((params) => {
@@ -235,7 +258,7 @@ connection.onDidCloseTextDocument((params) => {
 
 // Make the text document manager listen on the connection
 // for open, change and close text document events
-documents.listen(connection);
+documents.listen(connection)
 
 // Listen on the connection
-connection.listen();
+connection.listen()
